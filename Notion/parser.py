0890f36f69a2e76b5fe0a0b6f0c570ee0e1ea258
Gus545 <gusbus2@gmail.com>
2025-04-28 21:11:00 -0400

    Implemented set parsing functionality for parser.py

--- Notion/parser.py
+++ Notion/parser.py
@@ -1,3 +1,5 @@
+from models import WorkoutSet
+
 def parse_set_data(data : dict) -> dict:
     """
     Function to parse set data from a dictionary.
@@ -8,5 +10,30 @@ def parse_set_data(data : dict) -> dict:
     Returns:
         dict: Parsed set data.
     """
-    pass
 
+    set_data = data.get("properties", {})
+    set_data = {
+        "workout_name": data.get("properties", {}).get("Workout Title", {}).get("select", {}).get("name"),
+        "weight": set_data.get("Weight", {}).get("number"),
+        "reps": set_data.get("Reps", {}).get("number"),
+        "exercise_id": set_data.get("Exercise Reference", {}).get("relation", [{}])[0].get("id", {}),
+        "set_number": set_data.get("Set #", {}).get("number"),
+        "date": data.get("properties", {}).get("Date", {}).get("date", {}).get("start")
+    }
+
+    set = WorkoutSet(**set_data)
+
+    return set.model_dump()
+
+def parse_exercise_data(data : dict) -> dict:
+    """
+    Function to parse exercise data from a dictionary.
+    
+    Args:
+        data (dict): The exercise data to parse.
+    
+    Returns:
+        dict: Parsed exercise data.
+    """
+
+    pass
