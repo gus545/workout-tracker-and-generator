e3278c678f4b78c5ecaaabf7ac39e642367e1931
Gus545 <gusbus2@gmail.com>
2025-05-05 12:04:15 -0400

    feat(SyncService): add basic custom error handling to SyncService
    - Modified both sync functions to exit gracefully upon catching any parsing or database related errors

--- sync_service.py
+++ sync_service.py
@@ -2,12 +2,15 @@ import shutil
 import os
 from datetime import datetime
 from Notion.fetcher import Fetcher
-from Notion.parser import parse_database_info, parse_data
+from Notion.parser import parse_data
 from database import DatabaseManager
 from models import KeyedModel, Exercise, WorkoutSet
 from dotenv import load_dotenv
 from Notion.setter import Setter
 import logging
+from notion_client.errors import APIResponseError
+import traceback
+from errors import TableNotFoundError, CompositeKeyError, DatabaseError, SyncError, log_error
 
 logger = logging.getLogger(__name__)
 
@@ -57,11 +60,11 @@ class SyncService:
 
         if db_name not in self.model_registry:
             logger.error(f"No model found for {db_name}.")
-            raise ValueError(f"No model found for {db_name}")
+            return
         
         model = self.model_registry[db_name]
 
-        if not self.database.table_exists(db_name):
+        if db_name not in self.database.db.tables():
             logger.info(f"Creating local table for {db_name}...")
             self.database.create_table(db_name, model, remote_id=db_id)
         
@@ -75,23 +78,36 @@ class SyncService:
                 logger.warning(f"No last sync time found for {db_name}. Using default...")
                 last_sync = DEFAULT_SYNC_TIME            
             logger.info(f"Last sync time: {last_sync}")
-        except ValueError as e:
-            logger.error(f"Error getting last sync time: {e}")
+        except DatabaseError as e:
+            log_error(logger, e)
             return
 
         # Fetch all pages from the Notion database
-        new_pages = self.fetcher.query_pages_by_last_edited_time(db_id, last_sync)
+        try:
+            new_pages = self.fetcher.query_pages_by_last_edited_time(db_id, last_sync)
+        except APIResponseError as e:
+            logger.error(f"An error occurred during an API call: {e}")
+            logger.error("Sync failed.")
+            return
         
         # Parse the database information
-        parsed_data = parse_data(new_pages, model)
+        try:
+            parsed_data = parse_data(new_pages, model)
+        except SyncError as e:
+            log_error(logger, e)
+            return
+        
+        
 
         # Process and store the fetched data in the local database
         logger.info(f"Adding {len(parsed_data)} new pages to the local database.")
-        self.database.add(db_name, parsed_data)
-        
+        failed = self.database.add(db_name, parsed_data)
+        logger.debug(f"Entries not added: {failed}")
         self.database.update_last_sync_time(db_name)
         self.last_sync_time = datetime.now()
-        print(f"Data synced successfully at {self.last_sync_time}.")
+        logger.info(f"Data synced successfully at {self.last_sync_time}.")
+
+
 
     def sync_local_to_remote(self, db_info):
         """
@@ -109,12 +125,13 @@ class SyncService:
             
             # Parse remote pages
             parsed_remote_data = parse_data(remote_pages, self.get_model(db_name))
-
+            
             to_add = self.database.get_new_entries(parsed_remote_data, db_name)
             
             if not to_add:
                 print("No new entries to add.")
                 return
+            
             for page in to_add:
                 model = self.get_model(db_name)
                 notion_page = model(**page).to_notion_format()
@@ -123,12 +140,21 @@ class SyncService:
                 logger.info(f"New page with id {new_page_id} added to Notion database.")
                 page["page_id"] = new_page_id
                 self.database.update(db_name, page)
-            
-
-
+        
+          
+        except APIResponseError as e:
+            logger.error(f"An error occurred during an API call: {e}")
+            logger.error("Sync failed.")
+            return
+        except SyncError as e:
+            log_error(logger, e)
+            return
         except Exception as e:
             print(f"Error syncing data: {e}")
-            raise
+            logger.error(traceback.format_exc())
+            logger.error("Sync failed.")
+            return
+
 
 
     def get_model(self, db_name) -> KeyedModel:
